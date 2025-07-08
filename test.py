4c045c29ecb5e44a57b35baea9a41008bba6e965
mohamedmabrouk-666 <MohamedMabroukRashad666666@gmail.com>
2025-07-08 12:17:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import subprocess
+# import subprocess
 
-def test_output():
-    result = subprocess.run(['python3', 'app.py'], stdout=subprocess.PIPE)
-    assert b"Hello from CI/CD" in result.stdout
+# def test_output():
+#     result = subprocess.run(['python3', 'app.py'], stdout=subprocess.PIPE)
+#     assert b"Hello from CI/CD" in result.stdout
